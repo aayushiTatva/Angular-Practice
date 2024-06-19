383e6f1b83bee491664f270ddf473abf036483b6
aayushiTatva <aayushi.dhruva@etatvasoft.com>
2024-06-19 16:59:49 +0530

    Routing

--- AngularProject/src/app/app.routes.ts
+++ AngularProject/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { IndexComponent } from './index/index.component';
+import { EditComponent } from './edit/edit.component';
+import { ViewComponent } from './post/view/view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path: "", redirectTo: "index", pathMatch: 'full'},
+    {
+        path: "create", component: CreateComponent
+    },
+    {
+        path: "index", component: IndexComponent
+    },
+    {
+        path: "edit", component: EditComponent
+    },
+    {
+        path: "view", component: ViewComponent
+    }
+];
---
+++ AngularProject/src/app/post/view/view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './view.component.html',
+  styleUrl: './view.component.css'
+})
+export class ViewComponent {
+
+}
